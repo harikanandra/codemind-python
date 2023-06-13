37382d2a78ce06f07ab48d375720cc9cd4a51eac
harika nandra <128826606+harikanandra@users.noreply.github.com>
2023-06-13 21:09:19 +0530

    n,m=map(int,input().split()) def compute_gcd(x, y):
       while(y):
           x, y = y, x % y
       return x
    
    # This function computes LCM
    def compute_lcm(x, y):
       lcm = (x*y)//compute_gcd(x,y)
       return lcm
    
    print(compute_lcm(n,m))

--- LCM.py
+++ LCM.py
@@ -1,7 +1,13 @@
-a,b=map(int,input().split())
-x=max(a,b)
-while x>0:
-    if x%a==0 and x%b==0:
-        print(x)
-        break
-    x=x+1
+n,m=map(int,input().split())
+def compute_gcd(x, y):
+
+   while(y):
+       x, y = y, x % y
+   return x
+
+# This function computes LCM
+def compute_lcm(x, y):
+   lcm = (x*y)//compute_gcd(x,y)
+   return lcm
+
+print(compute_lcm(n,m))
